84105b5ac79d4d5b79570f78e34c8badda359f31
emi85 <emicam85@hotmail.com>
2013-10-16 16:54:15 -0300

    back to top
    back to top

--- js/general.js
+++ js/general.js
@@ -31,4 +31,22 @@ $(document).ready(function(){
 	$("a#go_cgreen").click(function() {
 		$('html,body').animate({scrollTop: $("#goto_green").offset().top},'slow');
 	});
+	
+	var offset = 220;
+	var duration = 500;
+	jQuery(window).scroll(function() {
+		if (jQuery(this).scrollTop() > offset) {
+			jQuery('.back-to-top').fadeIn(duration);
+		} else {
+			jQuery('.back-to-top').fadeOut(duration);
+		}
+	});
+	
+	jQuery('.back-to-top').click(function(event) {
+		event.preventDefault();
+		jQuery('html, body').animate({scrollTop: 0}, duration);
+		return false;
+	})
+
+	
 });
